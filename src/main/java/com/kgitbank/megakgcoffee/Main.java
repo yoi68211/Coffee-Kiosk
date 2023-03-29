4dfd94fa86b1c59dc58aaefc87c3cc9180c1624b
mafia917 <73516275+mafia917@users.noreply.github.com>
2023-03-29 11:18:54 +0900

    home뷰 업데이트

--- src/main/java/com/kgitbank/megakgcoffee/Main.java
+++ src/main/java/com/kgitbank/megakgcoffee/Main.java
@@ -1,21 +1,33 @@
 package com.kgitbank.megakgcoffee;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 
 public class Main extends Application {
 
     @Override
     public void init() {
+        System.out.println("test_gunwoo.");
 
     }
 
     @Override
     public void start(Stage stage) throws Exception {
 
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Main.fxml"));
+        Parent main = loader.load();
+
+        Scene scene = new Scene(main);
+        stage.setTitle("MEGA KG Coffee");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
-
+        launch(args);
     }
 }
