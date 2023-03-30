9382a8834f777aa3dd096df03f8537d4dd8e5a7d
mafia917 <73516275+mafia917@users.noreply.github.com>
2023-03-30 13:26:05 +0900

    머지

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -12,6 +12,7 @@ module com.kgitbank.megakgcoffee {
 
     opens com.kgitbank.megakgcoffee.Controller.Test to javafx.fxml;
     exports com.kgitbank.megakgcoffee.Controller.Test;
+    exports com.kgitbank.megakgcoffee.Controller.Orders;
 
     exports com.kgitbank.megakgcoffee.Controller.HomeView;
     opens com.kgitbank.megakgcoffee.Controller.HomeView to javafx.fxml;
@@ -21,6 +22,4 @@ module com.kgitbank.megakgcoffee {
     opens com.kgitbank.megakgcoffee.Model.DTO.HomeView to javafx.fxml;
     exports com.kgitbank.megakgcoffee.Service.HomeView;
     opens com.kgitbank.megakgcoffee.Service.HomeView to javafx.fxml;
-    exports com.kgitbank.megakgcoffee.Controller.Test;
-    exports com.kgitbank.megakgcoffee.Controller.Orders;
 }
